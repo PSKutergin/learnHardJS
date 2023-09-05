d5163a1a61d73a41e36a649a3a28005b9326db20
PSKutergin <PSKutergin@gmail.com>
2023-09-05 23:58:19 +0300

    finish hard lesson04

--- js/scripts.js
+++ js/scripts.js
@@ -1,117 +1,12 @@
-// Lesson 2
-const num = 266219
+'use strict'
 
-//Сумма всех цифр числа
-function getNumbersMulti(num) {
-  return num
-    .toString()
-    .split('')
-    .reduce(function (a, b) {
-      return +a * +b
-    })
-}
-
-//Возведение в степень
-const newNum = getNumbersMulti(num) ** 3
-
-//Вывод в консоль первых двух цифр числа
-console.log(newNum.toString().slice(0, 2))
-
-// Lesson 3
-function daysWeekIfElse(lang) {
-  if (lang === 'ru') {
-    console.log([
-      'Понедельник',
-      'Вторник',
-      'Среда',
-      'Четверг',
-      'Пятница',
-      'Суббота',
-      'Воскресенье',
-    ])
-  } else if (lang === 'en') {
-    console.log([
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-      'Sunday',
-    ])
+// Lesson 4
+const getStr = function (str) {
+  if (typeof str !== 'string') {
+    return 'В функцию передана не строка'
+  } else {
+    return str.trim().length > 30
+      ? str.trim().substr(0, 30) + '...'
+      : str.trim()
   }
 }
-
-const daysWeekSwitch = (lang) => {
-  switch (lang) {
-    case 'ru':
-      console.log([
-        'Понедельник',
-        'Вторник',
-        'Среда',
-        'Четверг',
-        'Пятница',
-        'Суббота',
-        'Воскресенье',
-      ])
-      break
-    case 'en':
-      console.log([
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-      ])
-      break
-  }
-}
-
-const daysWeekArr = (lang) =>
-  ({
-    en: [
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-      'Sunday',
-    ],
-    ru: [
-      'Понедельник',
-      'Вторник',
-      'Среда',
-      'Четверг',
-      'Пятница',
-      'Суббота',
-      'Воскресенье',
-    ],
-  }[lang])
-
-let lang = 'ru'
-daysWeekIfElse(lang)
-daysWeekSwitch(lang)
-console.log(daysWeekArr(lang))
-
-lang = 'en'
-daysWeekIfElse(lang)
-daysWeekSwitch(lang)
-console.log(daysWeekArr(lang))
-
-function getMajor(namePerson) {
-  namePerson === 'Артем'
-    ? console.log('директор')
-    : namePerson === 'Александр'
-    ? console.log('преподаватель')
-    : console.log('студент')
-}
-
-let namePerson = 'Артем'
-getMajor(namePerson)
-namePerson = 'Александр'
-getMajor(namePerson)
-namePerson = 'Игорь'
-getMajor(namePerson)
