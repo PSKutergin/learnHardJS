98327add964496bdb3eef314c0a46435171404fb
PSKutergin <PSKutergin@gmail.com>
2023-09-07 06:31:32 +0300

    finish hard lesson05

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 'use strict'
 
-// Lesson 4
-const getStr = function (str) {
-  if (typeof str !== 'string') {
-    return 'В функцию передана не строка'
-  } else {
-    return str.trim().length > 30
-      ? str.trim().substr(0, 30) + '...'
-      : str.trim()
+// Lesson 5
+let arr = [
+  '7364637',
+  '294834',
+  '3207552',
+  '4304380',
+  '194313',
+  '2095587',
+  '0358023',
+]
+
+arr.forEach((e) => {
+  if (+e[0] === 2 || +e[0] === 4) {
+    console.log(e)
+  }
+})
+
+for (let i = 2; i <= 100; i++) {
+  let count = 0
+  for (let j = 2; j <= i && count < 2; j++) {
+    if (i % j === 0) {
+      count++
+    }
+  }
+  if (count < 2) {
+    console.log(i)
   }
 }
