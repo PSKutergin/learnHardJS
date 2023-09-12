71c2660a339b440976b3466ff7879bdabac0b531
PSKutergin <PSKutergin@gmail.com>
2023-09-12 17:38:32 +0300

    finish hard lesson 9

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,40 @@
 'use strict'
 
-// Lesson 5
-let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-let today = week[new Date().getDay() - 1];
+// Lesson 9
+function update() {
+  const clock = document.getElementById('clock')
+  const fullDate = document.getElementById('full-date')
+  const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+  const monthNames = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
-for (let i = 0; i < week.length; i++) {
-  let div = document.createElement('div');
-  div.innerHTML = week[i];
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth();
+  let monthName = monthNames[month - 1]
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let today = week[date.getDay() - 1];
+  let hoursText
 
-  if (week[i] === today) {
-    div.style.fontWeight = "bold"
-  } else if (week[i] === 'Суббота' || week[i] === 'Воскресенье') {
-    div.style.fontStyle = "italic"
+  if (hours === 1 || hours === 21) {
+    hoursText = "час"
+  } else if ((hours >= 2 && hours <= 4) || (hours >= 22 && hours <= 24)) {
+    hoursText = "часа"
+  } else {
+    hoursText = "часов"
   }
-  document.body.insertAdjacentElement('beforebegin', div);
+
+  fullDate.textContent = `Сегодня ${today}, ${day} ${monthName} ${year} года, ${hours} ${hoursText} ${minutes} минут ${seconds} секунды`
+
+  if (day < 10) day = '0' + day
+  if (month < 10) month = '0' + month
+  if (hours < 10) hours = '0' + hours
+  if (minutes < 10) minutes = '0' + minutes
+  if (seconds < 10) seconds = '0' + seconds
+
+  clock.textContent = `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`;
 }
+
+setInterval(update, 1000)
